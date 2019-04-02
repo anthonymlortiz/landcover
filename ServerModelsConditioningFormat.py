29b029639f4cb627ab9b2376f64cd8b39469ef90
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-01 17:21:00 -0700

    "back end fix"

--- ServerModelsConditioningFormat.py
+++ ServerModelsConditioningFormat.py
@@ -23,7 +23,7 @@ class GnPytorchModel(BackendModel):
         inf_framework.load_model(self.model_fn)
         y_hat = inf_framework.predict_entire_image_gammas(naip_tile, gammas, betas)
         output = y_hat[:, :, 1:5]
-        return np.argmax(output, axis=2)
+        return output
 
 class InferenceFramework():
     def __init__(self, model, opts):
