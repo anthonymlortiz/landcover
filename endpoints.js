190c7fd5ff9dd490943bfe7c441bfa8a77d66659
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-02 11:35:04 -0700

    "adding cond superres"

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,4 @@
 ENDPOINTS = [
     {"name": "Group norm params exposed", "url": "http://40.121.17.50:4444/"},
-    {"name": "cond Fusionnet", "url": "http://40.121.17.50:4445/"}
+    {"name": "cond Fusionnet", "url": "http://52.170.211.35:4445/"}
 ];
