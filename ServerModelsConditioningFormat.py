c946463b0d8bdb3c3d24c1ec5023e68eb263741d
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-03 09:18:10 -0700

    "adding dropout"

--- ServerModelsConditioningFormat.py
+++ ServerModelsConditioningFormat.py
@@ -65,7 +65,7 @@ class InferenceFramework():
 
     def cunet_chip(self, x):
         _, w, h = x.shape
-        in_dim = 236
+        in_dim = 348
         out_dim = in_dim - 184
         chips = []
         n = int((w-184)/out_dim)
@@ -85,7 +85,7 @@ class InferenceFramework():
     def cunet_stitch_mask(self, y_hat_c, w, h):
         [img_width, img_height] = [w, h]
         mask = np.zeros([5, img_width, img_height])
-        in_dim = 236
+        in_dim = 348
         out_dim = in_dim - 184
         n = int(w / out_dim)
         quarter = 0
