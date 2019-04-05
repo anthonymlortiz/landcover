02aaf5d075dafa19788481a989ab1b1e71218bf7
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-04 17:21:39 -0700

    "fixing gn dropout"

--- ServerModelsConditioningFormat.py
+++ ServerModelsConditioningFormat.py
@@ -314,34 +314,49 @@ class InferenceFramework():
             self.model.cuda()
         x = np.rollaxis(x, 2, 1)
         x = x[:4, :, :]
-        norm_image = x / 255.0
-        _, w, h = norm_image.shape
+        naip_tile = x / 255.0
+
+        down_weight_padding = 40
+        height = naip_tile.shape[0]
+        width = naip_tile.shape[1]
+
+        stride_x = self.input_size - down_weight_padding * 2
+        stride_y = self.input_size - down_weight_padding * 2
+
+        output = np.zeros((height, width, self.output_channels), dtype=np.float32)
+        counts = np.zeros((height, width), dtype=np.float32) + 0.000000001
+        kernel = np.ones((self.input_size, self.input_size), dtype=np.float32) * 0.1
+        kernel[10:-10, 10:-10] = 1
+        kernel[down_weight_padding:down_weight_padding + stride_y,
+        down_weight_padding:down_weight_padding + stride_x] = 5
+
+        batch = []
+        batch_indices = []
+        batch_count = 0
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        out = np.zeros((5,w,h))
-        norm_image1 = norm_image[:, :w-(w%128), :h-(h%128)]
-        norm_image2 = norm_image[:, (w % 128):w, (h % 128):h ]
-        norm_image3 = norm_image[:, :w - (w % 128), (h % 128):h]
-        norm_image4 = norm_image[:, (w % 128):w, :h - (h % 128)]
-        x_c_tensor1 = torch.from_numpy(norm_image1).float().to(device)
-       # x_c_tensor2 = torch.from_numpy(norm_image2).float().to(device)
-       # x_c_tensor3 = torch.from_numpy(norm_image3).float().to(device)
-       # x_c_tensor4 = torch.from_numpy(norm_image4).float().to(device)
-        y_pred1 = self.fusionnet_gn_fun(x_c_tensor1.unsqueeze(0), gammas, betas, dropouts)
-        #y_pred2 = self.fusionnet_gn_fun(x_c_tensor2.unsqueeze(0), gammas, betas, dropouts)
-        #y_pred3 = self.fusionnet_gn_fun(x_c_tensor3.unsqueeze(0), gammas, betas, dropouts)
-       # y_pred4 = self.fusionnet_gn_fun(x_c_tensor4.unsqueeze(0), gammas, betas, dropouts)
-        y_hat1 = (Variable(y_pred1).data).cpu().numpy()
-       # y_hat2 = (Variable(y_pred2).data).cpu().numpy()
-        #y_hat3 = (Variable(y_pred3).data).cpu().numpy()
-       # y_hat4 = (Variable(y_pred4).data).cpu().numpy()
-        out[:, :w - (w % 128), :h - (h % 128)] = y_hat1
-       # out[:, (w % 128):w, (h % 128):h ] = y_hat2
-       # out[:, :w - (w % 128), (h % 128):h] = y_hat3
-       # out[:, (w % 128):w, :h - (h % 128)] = y_hat4
-        pred = np.rollaxis(out, 0, 3)
-        pred = np.moveaxis(pred, 0, 1)
-        print(pred.shape)
-        return pred
+
+        for y_index in (list(range(0, height - self.input_size, stride_y)) + [height - self.input_size, ]):
+            for x_index in (list(range(0, width - self.input_size, stride_x)) + [width - self.input_size, ]):
+                naip_im = naip_tile[y_index:y_index + self.input_size, x_index:x_index + self.input_size, :]
+
+                batch.append(naip_im)
+                batch_indices.append((y_index, x_index))
+                batch_count += 1
+        batch = torch.from_numpy(batch).float().to(device)
+        model_output = self.fusionnet_gn_fun(batch, gammas, betas, dropouts)
+        model_output = (Variable(model_output).data).cpu().numpy()
+        for i, (y, x) in enumerate(batch_indices):
+            output[y:y + self.input_size, x:x + self.input_size] += model_output[i] * kernel[..., np.newaxis]
+            counts[y:y + self.input_size, x:x + self.input_size] += kernel
+
+        output = output / counts[..., np.newaxis]
+        output[:, :, 4] += output[:, :, 5]
+        output[:, :, 4] += output[:, :, 6]
+        output = output[:, :, 1:5]
+        output = np.rollaxis(output, 0, 3)
+        output = np.moveaxis(output, 0, 1)
+        return output
+
 
     def predict_entire_image_gammas_unet(self, x, gammas, betas, dropouts):
         x = np.swapaxes(x, 0, 2)
