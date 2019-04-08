17aab0f0b6c4a9f43c137a17ba24a37920979fa2
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-08 11:25:55 -0700

    "fixing gn dropout"

--- fusionnet.py
+++ fusionnet.py
@@ -171,7 +171,7 @@ class GroupNorm(nn.Module):
         mean = x.mean(-1, keepdim=True)
         var = x.var(-1, keepdim=True)
         print(var)
-        var[:,:,:] = torch.ones(var.shape) * 241300
+        var[:,:,:] = torch.ones(var.shape) * 2e11
 
         x = (x) / (var+self.eps).sqrt()
         x = x.view(N,C,H,W)
