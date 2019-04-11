d22a81b78e3ecfdd9f30b88cd50625ff464a1b40
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-11 09:36:24 -0700

    "updating backend"

--- endpoints.js
+++ endpoints.js
@@ -1,4 +1,5 @@
 ENDPOINTS = [
-    {"name": "Isotropic Group norm params exposed", "url": "http://isotropicgn.eastus.cloudapp.azure.com:4444/"}
+    {"name": "Isotropic Group norm finetunned", "url": "http://isotropicgn.eastus.cloudapp.azure.com:4444/"},
+    {"name": "Isotropic Group norm params exposed", "url": "http://anthonyvm1.eastus.cloudapp.azure.com:4444/"}
 
 ];
