6895aa05059622ffbf73369f4b7a043112aa5474
anthonymlortiz <anthonymlortiz@gmail.com>
2019-04-11 10:13:04 -0700

    "updating backend"

--- run_server_tuned.sh
+++ run_server_tuned.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 MODEL="tuned_unet"
-MODEL_FN="/mnt/blobfuse/train-output/conditioning/models/finetuning/finetuned_unet_gn_500samples.pth.tar"
+MODEL_FN="/mnt/blobfuse/train-output/conditioning/models/finetuning/finetuned_unet_gn_1000samples.pth.tar"
 
 python -u backend_server.py \
     --model ${MODEL} \
